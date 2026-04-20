decfa83a99fe7add35fdd343f6cfb84cfe40df31
Jeron Reijne <57570864+ItsJepoan@users.noreply.github.com>
2026-04-20 13:41:11 +0200

    First Test Website

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const roles = [
+  {
+    name: "Dorpsbewoner",
+    description: "Een gewone inwoner van Whatsapperdam zonder speciale krachten. Werk samen met het dorp om de weerwolven te vinden.",
+    image: "images/dorpsbewoner.jpg",
+    type: "Dorp"
+  },
+  {
+    name: "Weerwolf",
+    description: "Probeert 's nachts slachtoffers te maken en overdag onschuldig over te komen. Werk samen met andere weerwolven.",
+    image: "images/weerwolf.jpg",
+    type: "Weerwolf"
+  },
+  {
+    name: "Ziener",
+    description: "Mag informatie verzamelen over andere spelers en helpt het dorp met inzicht en strategie.",
+    image: "images/ziener.jpg",
+    type: "Dorp"
+  }
+];
