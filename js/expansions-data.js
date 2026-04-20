b1fb03941fafd42018a0df47a61d17d3f687844d
Jeron Reijne <57570864+ItsJepoan@users.noreply.github.com>
2026-04-21 00:04:12 +0200

    ?

--- js/expansions-data.js
+++ js/expansions-data.js
@@ -1,12 +1,7 @@
 const expansions = [
   {
-    key: "dark-forest",
-    name: "Donker Woud",
-    description: "Voorbeeldtekst: deze uitbreiding voegt mysterieuze en gevaarlijke rollen toe aan het spel."
+    key: "de-dorpsbrouwerij",
+    name: "De Dorpsbrouwerij",
+    description: "In sommige spellen opent in Whatsapperdam de Dorpsbrouwerij. De lokale kroeg zorgt voor gezelligheid, maar ook voor een klein probleem: sommige dorpsbewoners drinken nét iets te veel.\nWanneer een speler boven het chatlimiet van 20 berichten uitkomt (zonder dat dit verdediging of direct wederwoord is), heeft die speler 50% kans om die nacht dronken te worden.\nEen dronken speler merkt hier zelf niets van, maar zijn acties kunnen anders uitpakken dan verwacht. De GM kan bijvoorbeeld bepalen dat een speler het verkeerde doelwit bezoekt, verkeerde informatie ontvangt of dat een actie op een andere speler terechtkomt. Rollen die personen kiezen, zoals de genezer, het sletje, de onderdrukker, de papegaai of grafrover kunnen hierdoor iemand anders raken. Rollen die informatie krijgen, zoals de ziener, vos of heks kunnen verkeerde informatie ontvangen. Zelfs een weerwolf kan per ongeluk bij het verkeerde huis eindigen.\nAlle rollen en hun dronken acties zijn te vinden in Appendix D: Dronken\nBij deze uitbreiding horen ook twee speciale rollen:"
   },
-  {
-    key: "chaos-night",
-    name: "Chaotische Nacht",
-    description: "Voorbeeldtekst: deze uitbreiding draait om onvoorspelbaarheid, extra spanning en verwarring."
-  }
 ];
