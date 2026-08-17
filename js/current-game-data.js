c28763807954bff6ef7e9175a51b7d55ce9efea4
Jeron Reijne <57570864+ItsJepoan@users.noreply.github.com>
2026-08-17 23:02:00 +0200

    nacht 0

--- js/current-game-data.js
+++ js/current-game-data.js
@@ -19,47 +19,179 @@ const currentGameBasisRoleIds = [];
 const currentGameBasisWerewolfRoleId = null;
 
 const currentGamePlayers = [
-  { name: "Julian", alive: true, circleOrder: 15 },
-  { name: "Solène", alive: true, circleOrder: 16 },
-  { name: "Ilse", alive: true, circleOrder: 13 },
-  { name: "Ruben", alive: true, circleOrder: 28 },
-  { name: "Nina", alive: true, circleOrder: 20 },
-  { name: "Nick", alive: true, circleOrder: 22 },
-  { name: "Barry", alive: true, circleOrder: 8 },
-  { name: "Per", alive: true, circleOrder: 17 },
-  { name: "Micha", alive: true, circleOrder: 9 },
-  { name: "Tijs", alive: true, circleOrder: 2 },
-  { name: "Ivar", alive: true, circleOrder: 4 },
-  { name: "Michel", alive: true, circleOrder: 5 },
-  { name: "Lucas", alive: true, circleOrder: 6 },
-  { name: "Lotte", alive: true, circleOrder: 10 },
-  { name: "Rick", alive: true, circleOrder: 21 },
-  { name: "Magnus", alive: true, circleOrder: 18 },
-  { name: "Nienke", alive: true, circleOrder: 27 },
-  { name: "Fajjaaz", alive: true, circleOrder: 14 },
-  { name: "Mats", alive: true, circleOrder: 3 },
-  { name: "Benji", alive: true, circleOrder: 23 },
-  { name: "Thomas", alive: true, circleOrder: 11 },
-  { name: "Donovan", alive: true, circleOrder: 19 },
-  { name: "Maik", alive: true, circleOrder: 26 },
-  { name: "Shannon", alive: true, circleOrder: 7 },
-  { name: "Kayen", alive: true, circleOrder: 25 },
-  { name: "Megan", alive: true, circleOrder: 12 },
-  { name: "Bouke", alive: true, circleOrder: 1 },
-  { name: "Pepijn", alive: true, circleOrder: 24 }
+  {
+    "name": "Julian",
+    "alive": true,
+    "circleOrder": 15
+  },
+  {
+    "name": "Solène",
+    "alive": false,
+    "circleOrder": 16
+  },
+  {
+    "name": "Ilse",
+    "alive": true,
+    "circleOrder": 13
+  },
+  {
+    "name": "Ruben",
+    "alive": true,
+    "circleOrder": 28
+  },
+  {
+    "name": "Nina",
+    "alive": true,
+    "circleOrder": 20
+  },
+  {
+    "name": "Nick",
+    "alive": true,
+    "circleOrder": 22
+  },
+  {
+    "name": "Barry",
+    "alive": true,
+    "circleOrder": 8
+  },
+  {
+    "name": "Per",
+    "alive": true,
+    "circleOrder": 17
+  },
+  {
+    "name": "Micha",
+    "alive": true,
+    "circleOrder": 9
+  },
+  {
+    "name": "Tijs",
+    "alive": true,
+    "circleOrder": 2
+  },
+  {
+    "name": "Ivar",
+    "alive": false,
+    "circleOrder": 4
+  },
+  {
+    "name": "Michel",
+    "alive": true,
+    "circleOrder": 5
+  },
+  {
+    "name": "Lucas",
+    "alive": true,
+    "circleOrder": 6
+  },
+  {
+    "name": "Lotte",
+    "alive": true,
+    "circleOrder": 10
+  },
+  {
+    "name": "Rick",
+    "alive": true,
+    "circleOrder": 21
+  },
+  {
+    "name": "Magnus",
+    "alive": true,
+    "circleOrder": 18
+  },
+  {
+    "name": "Nienke",
+    "alive": true,
+    "circleOrder": 27
+  },
+  {
+    "name": "Fajjaaz",
+    "alive": true,
+    "circleOrder": 14
+  },
+  {
+    "name": "Mats",
+    "alive": true,
+    "circleOrder": 3
+  },
+  {
+    "name": "Benji",
+    "alive": true,
+    "circleOrder": 23
+  },
+  {
+    "name": "Thomas",
+    "alive": true,
+    "circleOrder": 11
+  },
+  {
+    "name": "Donovan",
+    "alive": true,
+    "circleOrder": 19
+  },
+  {
+    "name": "Maik",
+    "alive": false,
+    "circleOrder": 26
+  },
+  {
+    "name": "Shannon",
+    "alive": true,
+    "circleOrder": 7
+  },
+  {
+    "name": "Kayen",
+    "alive": false,
+    "circleOrder": 25
+  },
+  {
+    "name": "Megan",
+    "alive": true,
+    "circleOrder": 12
+  },
+  {
+    "name": "Bouke",
+    "alive": true,
+    "circleOrder": 1
+  },
+  {
+    "name": "Pepijn",
+    "alive": false,
+    "circleOrder": 24
+  }
 ];
 
 const currentGameAnnouncements = [
-  { datetime: "2026-08-16T09:00", label: "16 augustus 09:00", message: "Rik is gestorven en was *De Burger*" },
-  { datetime: "2026-08-16T20:00", label: "16 augustus 20:00", message: "Riek is op de brandstapel gegooid en was *De Weerwolf*" },
-  { datetime: "2026-08-17T09:00", label: "17 augustus 09:00", message: "Ricknald is gestorven en was *Cupido*" },
-  { datetime: "2026-08-17T20:00", label: "17 augustus 20:00", message: "Riq is op de brandstapel gegooid en was *De Ziener*" },
-  { datetime: "2026-08-18T09:00", label: "18 augustus 09:00", message: "Rickolas is gestorven en was *De Jager*" },
-  { datetime: "2026-08-18T20:00", label: "18 augustus 20:00", message: "Rickerik is op de brandstapel gegooid en was *De Dorpsgek*" },
-  { datetime: "2026-08-19T09:00", label: "19 augustus 09:00", message: "Rieck is gestorven en was *De Genezer*" },
-  { datetime: "2026-08-19T20:00", label: "19 augustus 20:00", message: "Rikkie is op de brandstapel gegooid en was *De Zondebok*" },
-  { datetime: "2026-08-20T09:00", label: "20 augustus 09:00", message: "Ricktoria is gestorven en was *De Dorpsoudste*" },
-  { datetime: "2026-08-20T20:00", label: "20 augustus 20:00", message: "Riqck is op de brandstapel gegooid en was *De Heks - Wakkerdam*" }
+  {
+    "datetime": "2026-08-17T00:00",
+    "label": "17 augustus",
+    "message": "Spel 15 is vandaag gestart."
+  },
+  {
+    "datetime": "2026-08-18T09:00:01",
+    "label": "18 augustus 09:00",
+    "message": "Kayen is verslonden door de weerwolven en was *De Jager*."
+  },
+  {
+    "datetime": "2026-08-18T09:00:02",
+    "label": "18 augustus 09:00",
+    "message": "Pepijn stierf aan liefdesverdriet om Kayen en was *De Wakkerdamse Toneelspeler*."
+  },
+  {
+    "datetime": "2026-08-18T09:00:03",
+    "label": "18 augustus 09:00",
+    "message": "Solène is verslonden door de weerwolven en was *De Stotterende Raadsheer*."
+  },
+  {
+    "datetime": "2026-08-18T09:00:04",
+    "label": "18 augustus 09:00",
+    "message": "Ivar stierf aan liefdesverdriet om Solène en was *De Dorpsgek*."
+  },
+  {
+    "datetime": "2026-08-18T09:00:05",
+    "label": "18 augustus 09:00",
+    "message": "Maik werd dodelijk geraakt door een kogel en was *De Zondebok*."
+  }
 ];
 
 const currentGameTalentNames = [];
