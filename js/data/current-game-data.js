ab4ddc9d14c9e17b399f73409516e26071463087
Jeron Reijne <57570864+ItsJepoan@users.noreply.github.com>
2026-09-11 16:41:45 +0200

    =-

--- js/data/current-game-data.js
+++ js/data/current-game-data.js
@@ -1,9 +1,13 @@
 const currentGameRoleIds = [
   "de-agent",
+  "de-alwetende-uil",
+  "de-bommenwerper",
   "de-burger",
   "de-burgerlijke-burger",
   "cupido",
+  "de-dictator",
   "de-dodenbezweerder",
+  "de-dokter",
   "de-dorpsgek",
   "de-dorpsoudste",
   "de-genezer",
@@ -11,58 +15,53 @@ const currentGameRoleIds = [
   "de-husselaar",
   "de-hypocriet",
   "de-jager",
+  "de-jaloerse-ex",
   "de-journalist",
   "de-leerling",
+  "de-onderduiker",
   "de-papegaai",
+  "de-rebel",
   "de-ridder-met-het-roestige-zwaard",
   "de-samurai",
-  "de-smid",
   "de-stalker",
   "de-stotterende-raadsheer",
-  "de-twijfelaar",
   "de-verkenner",
   "de-verwarde-ziener",
   "de-vos",
-  "de-vriend",
-  "de-waakengel",
   "de-wachter",
   "de-ziener",
   "de-zondebok",
-  "het-toegewijde-dienstmeisje",
-  "harige-harry",
-  "het-medium",
   "het-onschuldige-sletje",
   "de-fluitspeler",
   "de-pyromaan",
-  "de-verloren-ziel",
   "de-heks",
   "de-kleine-wilde",
   "de-onderdrukker",
   "de-weerwolf",
+  "de-besmettelijke-raaf",
   "de-twee-gescheiden-gezusters",
   "de-witte-weerwolf",
-  "de-glitterende-toverwolf",
+  "de-besmettelijke-oerwolf",
+  "de-concierge",
   "de-jachtwolf",
   "de-slager",
-  "de-verdoemde",
   "de-welp",
   "de-wolf-in-schaapskleren",
   "de-burgemeester",
   "de-geliefde",
+  "de-verlammer",
   "de-feestwolf",
   "de-illusionist",
   "het-wolfsspook",
   "de-wolfsoudste",
   "de-wingman",
+  "de-onzekere-grenswachter",
   "de-klier",
   "de-omroeper",
   "de-premiejager",
-  "de-vervloekte-wolf",
   "de-boze-wolf",
-  "de-wolfsschrijver",
   "de-charmante-dealer",
   "de-aasgier",
-  "de-sekteleider",
   "de-betoverde",
   "de-herrezene",
   "de-onderdrukte",
@@ -72,159 +71,34 @@ const currentGameRoleIds = [
   "de-droomwever",
   "de-fotograaf",
   "de-verdwaalde-wolf",
-  "de-dief",
-  "de-twee-gezusters",
-  "de-drie-gebroeders"
+  "de-dief"
 ];
 
-const currentGameNumber = 16;
+const currentGameNumber = 17;
 
 const currentGameMasters = [
-  "Rick"
+  "Kayen"
 ];
 
 const activeExpansionKeys = [
-  "de-dorpsbrouwerij",
-  "de-boeren",
-  "basis-rollen",
-  "de-cirkelzitting",
-  "dronkenschap"
+  "de-tongtovenaar",
+  "dronkenschap",
+  "de-adellijke-raad"
 ];
 
 const activeExpansionVariants = {};
 
 const excludedActiveExpansionRoleIds = [];
 
-const currentGameBasisRoleIds = [
-  "cupido",
-  "de-genezer",
-  "de-fotograaf",
-  "de-vriend",
-  "de-jager",
-  "de-burger",
-  "barman-bob",
-  "barman-bert",
-  "de-dief"
-];
+const currentGameBasisRoleIds = [];
 
 const currentGameBasisWerewolfRoleId = null;
 
 const currentGamePlayers = [
   {
-    "name": "Jeron",
-    "alive": true,
-    "circleOrder": 6
-  },
-  {
-    "name": "Daphne",
-    "alive": true,
-    "circleOrder": 24
-  },
-  {
-    "name": "Julian",
-    "alive": true,
-    "circleOrder": 3
-  },
-  {
-    "name": "Lara",
-    "alive": true,
-    "circleOrder": 18
-  },
-  {
-    "name": "Nina",
-    "alive": true,
-    "circleOrder": 9
-  },
-  {
-    "name": "Pepijn",
-    "alive": true,
-    "circleOrder": 21
-  },
-  {
-    "name": "Jolly",
-    "alive": true,
-    "circleOrder": 14
-  },
-  {
-    "name": "Nick",
-    "alive": true,
-    "circleOrder": 27
-  },
-  {
-    "name": "Maik",
-    "alive": true,
-    "circleOrder": 5
-  },
-  {
-    "name": "Solène",
-    "alive": true,
-    "circleOrder": 16
-  },
-  {
-    "name": "Ruben",
-    "alive": true,
-    "specialStatus": "boeren-burgemeester",
-    "circleOrder": 22
-  },
-  {
-    "name": "Tijs",
-    "alive": true,
-    "circleOrder": 10
-  },
-  {
-    "name": "Mats",
-    "alive": true,
-    "circleOrder": 1
-  },
-  {
-    "name": "Micha",
-    "alive": true,
-    "circleOrder": 25
-  },
-  {
-    "name": "Per",
-    "alive": true,
-    "circleOrder": 8
-  },
-  {
-    "name": "Kayen",
-    "alive": true,
-    "circleOrder": 19
-  },
-  {
-    "name": "Fajjaaz",
-    "alive": true,
-    "circleOrder": 29
-  },
-  {
-    "name": "Magnus",
-    "alive": true,
-    "circleOrder": 7
-  },
-  {
     "name": "Barry",
     "alive": true,
-    "circleOrder": 23
-  },
-  {
-    "name": "Donovan",
-    "alive": true,
-    "circleOrder": 11
-  },
-  {
-    "name": "Michel",
-    "alive": false,
-    "circleOrder": 20
-  },
-  {
-    "name": "Bouke",
-    "alive": true,
-    "circleOrder": 26
-  },
-  {
-    "name": "Nienke",
-    "alive": true,
-    "circleOrder": 15
+    "circleOrder": 1
   },
   {
     "name": "Benji",
@@ -232,80 +106,128 @@ const currentGamePlayers = [
     "circleOrder": 2
   },
   {
-    "name": "Megan",
+    "name": "Bouke",
+    "alive": true,
+    "circleOrder": 3
+  },
+  {
+    "name": "Donovan",
+    "alive": true,
+    "circleOrder": 4
+  },
+  {
+    "name": "Fajjaaz",
+    "alive": true,
+    "specialStatus": "twijfelt",
+    "circleOrder": 5
+  },
+  {
+    "name": "Ilse",
+    "alive": true,
+    "circleOrder": 6
+  },
+  {
+    "name": "Jeron",
+    "alive": true,
+    "circleOrder": 7
+  },
+  {
+    "name": "Jolly",
+    "alive": true,
+    "circleOrder": 8
+  },
+  {
+    "name": "Julian",
+    "alive": true,
+    "specialStatus": "twijfelt",
+    "circleOrder": 9
+  },
+  {
+    "name": "Lotte",
+    "alive": true,
+    "circleOrder": 10
+  },
+  {
+    "name": "Magnus",
+    "alive": true,
+    "circleOrder": 11
+  },
+  {
+    "name": "Maik",
+    "alive": true,
+    "circleOrder": 12
+  },
+  {
+    "name": "Mats",
     "alive": true,
     "circleOrder": 13
   },
   {
-    "name": "Niels",
+    "name": "Megan",
     "alive": true,
-    "circleOrder": 28
+    "circleOrder": 14
   },
   {
-    "name": "Ivar",
+    "name": "Micha",
     "alive": true,
+    "circleOrder": 15
+  },
+  {
+    "name": "Michel",
+    "alive": true,
+    "circleOrder": 16
+  },
+  {
+    "name": "Nick",
+    "alive": true,
+    "specialStatus": "twijfelt",
     "circleOrder": 17
+  },
+  {
+    "name": "Nienke",
+    "alive": true,
+    "circleOrder": 18
+  },
+  {
+    "name": "Nina",
+    "alive": true,
+    "circleOrder": 19
+  },
+  {
+    "name": "Pepijn",
+    "alive": true,
+    "circleOrder": 20
+  },
+  {
+    "name": "Per",
+    "alive": true,
+    "circleOrder": 21
+  },
+  {
+    "name": "Ruben",
+    "alive": true,
+    "specialStatus": "twijfelt",
+    "circleOrder": 22
   },
   {
     "name": "Shannon",
     "alive": true,
-    "circleOrder": 4
+    "specialStatus": "twijfelt",
+    "circleOrder": 23
+  },
+  {
+    "name": "Solène",
+    "alive": true,
+    "circleOrder": 24
+  },
+  {
+    "name": "Tijs",
+    "alive": true,
+    "circleOrder": 25
   }
 ];
 
-const currentGameAnnouncements = [
-  {
-    "datetime": "2026-09-01T11:34:00",
-    "label": "1 september 11:34",
-    "message": "Ruben is Burgemeester geworden!"
-  },
-  {
-    "datetime": "2026-09-01T09:00:00",
-    "label": "1 september 09:00",
-    "message": "Michel is opgegeten door de wolven en was *De Pyromaan*."
-  },
-  {
-    "datetime": "2026-08-31T20:00:00",
-    "label": "31 augustus 20:00",
-    "message": "Spel 16 is gestart!",
-    "kind": "game-start"
-  },
-  {
-    "datetime": "2026-08-31T14:00:04",
-    "label": "31 augustus 14:00",
-    "message": "Lucas heeft toch het dorp verlaten."
-  },
-  {
-    "datetime": "2026-08-31T14:00:01",
-    "label": "31 augustus 14:00",
-    "message": "Lotte heeft het dorp verlaten."
-  },
-  {
-    "datetime": "2026-08-31T14:00:02",
-    "label": "31 augustus 14:00",
-    "message": "Niels koos voor het dorp en is herrezen."
-  },
-  {
-    "datetime": "2026-08-31T14:00:03",
-    "label": "31 augustus 14:00",
-    "message": "Shannon koos voor het dorp en is herrezen."
-  },
-  {
-    "datetime": "2026-08-30T21:00:01",
-    "label": "30 augustus 21:00",
-    "message": "Niels is gestorven door te lang twijfelen."
-  },
-  {
-    "datetime": "2026-08-30T21:00:02",
-    "label": "30 augustus 21:00",
-    "message": "Lotte is gestorven door te lang twijfelen."
-  },
-  {
-    "datetime": "2026-08-30T21:00:03",
-    "label": "30 augustus 21:00",
-    "message": "Shannon is gestorven door te lang twijfelen."
-  }
-];
+const currentGameAnnouncements = [];
 
 const currentGameTalentNames = [];
 
--- js/data/expansions-data.js
+++ js/data/expansions-data.js
@@ -185,6 +185,13 @@ const expansions = [
     "category": "uitbreiding"
   },
   {
+    "key": "de-tongtovenaar",
+    "name": "De Tongtovenaar",
+    "shortDescription": "Een uitbreiding waarin taal, regels en gesprekken dagelijks vervormd worden.",
+    "description": "Met natte staf en vervloekte tong heerst de Tongtovenaar over de woorden van Whatsapperdam 👅\n\nWaar hij verschijnt, verandert taal in chaos. Geen gesprek blijft veilig, geen zin blijft normaal. Het dorp buigt zich dagelijks onder nieuwe vreemde spreuken terwijl iedereen wanhopig probeert elkaar nog te begrijpen.\n\nDe Tongtovenaar hoort bij de burgers.\n\nTussen 10:00 en 09:00 mag hij via een direct bericht aan de GM één Tongspreuk kiezen die actief wordt over het dorp. Hij mag kiezen uit bestaande spreuken of zelf een nieuwe bedenken, zolang deze eerst door de GM wordt goedgekeurd.\n\nDeze spreuken veranderen de manier waarop spelers mogen praten. Soms worden berichten beperkt. Soms vervormd. Soms compleet op hun kop gezet.\n\nDe Tongtovenaar mag zijn krachten nooit gebruiken om bewust één alliantie te bevoordelen. Iedere spreuk moet eerlijk gelden voor het hele dorp: burgers, weerwolven en onafhankelijken.\n\nZelfs na zijn dood blijft zijn stem nog door Whatsapperdam galmen. Ook dan mag hij nieuwe spreuken blijven uitspreken.",
+    "category": "uitbreiding"
+  },
+  {
     "key": "de-talentvolle-burgers",
     "name": "De Talentvolle Burgers",
     "talentsUrl": "talenten.html",
